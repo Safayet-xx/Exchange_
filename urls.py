977db753649d5e1583609c8af012860049c5a85a
Vaishnavi M <vaishnavichettiar2002@gmail.com>
2025-12-29 23:10:22 +0000

    Connect post detail to booking flow

--- urls.py
+++ urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = "exchanges"
+app_name = 'posts'
 
 urlpatterns = [
-    # List and create
-    path("", views.session_list_view, name="list"),
-    path("create/", views.create_session_view, name="create"),
-    
-    # Session detail and actions
-    path("<int:pk>/", views.session_detail_view, name="detail"),
-    path("<int:pk>/accept/", views.accept_session_view, name="accept"),
-    path("<int:pk>/complete/", views.complete_session_view, name="complete"),
-    path("<int:pk>/cancel/", views.cancel_session_view, name="cancel"),
+    path('', views.post_list, name='list'),
+    path('create/', views.post_create, name='create'),
+    path('my/', views.my_posts, name='my_posts'),
+    path('<int:pk>/', views.post_detail, name='detail'),
+    path('<int:pk>/delete/', views.post_delete, name='delete'),
 ]
